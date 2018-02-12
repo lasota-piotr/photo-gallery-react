5976138e07d8ab73fed7d4be3cd74bf101e94ca3
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-12 21:21:37 +0100

    add babel and react

--- webpack.common.js
+++ webpack.common.js
@@ -6,10 +6,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
     entry: {
       app: './src/index.js'
     },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   plugins: [
       new CleanWebpackPlugin(['dist']),
       new HtmlWebpackPlugin({
-          title: 'Production'
+        title: 'Production',
+        template: 'index.html'
     })
   ],
   output: {
