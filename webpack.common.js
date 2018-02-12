00e8a08ebb866a94c17de69a3344490d507766c9
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-12 22:19:40 +0100

    Add scss to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,28 +1,56 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-  module.exports = {
-    entry: {
-      app: './src/index.js'
-    },
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        title: 'Production',
-        template: 'index.html'
-    })
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: 'index.html',
+    }),
+    extractSass,
   ],
   output: {
-      filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
