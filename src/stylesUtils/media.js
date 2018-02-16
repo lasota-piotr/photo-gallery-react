0352c6e58edea037fb93cf7ebaa6e95130bf3d52
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-16 16:28:23 +0100

    Add media query helper for styled-components

---
+++ src/stylesUtils/media.js
@@ -0,0 +1,17 @@
+import { css } from 'styled-components';
+import { mqBreakPoints as sizes } from './styleConstants';
+
+// iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16;
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)};
+    }
+  `;
+  return accumulator;
+}, {});
+
+export default media;
---
+++ src/stylesUtils/styleConstants.js
@@ -0,0 +1,6 @@
+export const mqBreakPoints = {
+  mobile: 320,
+  tablet: 740,
+  desktop: 980,
+  wide: 1300,
+};
