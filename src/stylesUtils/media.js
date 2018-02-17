b8326d0fdbf35161dd0f2ab9e621d6c95818b59e
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 19:54:38 +0100

    Add example usage in comment

--- src/stylesUtils/media.js
+++ src/stylesUtils/media.js
@@ -1,7 +1,18 @@
 import { css } from 'styled-components';
 import { mqBreakPoints as sizes } from './styleConstants';
 
-// iterate through the sizes and create a media template
+/*
+* iterate through the sizes and create a media template
+* Usage:
+const Container = styled.div`
+  color: #333;
+  ${media.desktop`padding: 0 20px;`}
+  ${media.tablet`padding: 0 10px;`}
+  ${media.phone`padding: 0 5px;`}
+`
+* */
+
+
 const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
