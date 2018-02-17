196e688a4568e298010daebaf48b7544cf3bae95
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 13:39:36 +0100

    Styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,64 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Logo = styled(Link)`
+  &:hover {
+    color: ${props => props.theme.colorGrayDarker};
+    text-decoration: none;
+  }
+`;
 
+const LogoText = styled.h2`
+  color: ${props => props.theme.colorText};
+  margin-bottom: 0;
+`;
+
+const HeaderLayout = styled.header`
+  display: flex;
+  align-items: center;
+  padding: ${props => props.theme.unit};
+  border-bottom: ${props => props.theme.border};
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  list-style: none;
+`;
+
+const activeClassName = 'is-active';
+
+const NavLinkItem = styled(NavLink).attrs({
+  activeClassName,
+})`
+  color: ${props => props.theme.colorText};
+  &:hover {
+    text-decoration: none;
+  }
+  &.${activeClassName} {
+    text-decoration: underline;
+  }
+`;
 
 const Header = () => (
-  <header>
-    <h2>Photoflix</h2>
-    <nav>
-      <ul>
+  <HeaderLayout>
+    <Logo to="/">
+      <LogoText>Photoflix</LogoText>
+    </Logo>
+    <Nav>
+      <NavList>
         <li>
-          <NavLink to="/" exact>
+          <NavLinkItem to="/" exact activeClassName={activeClassName}>
             Home
-          </NavLink>
+          </NavLinkItem>
         </li>
-      </ul>
-    </nav>
-  </header>
+      </NavList>
+    </Nav>
+  </HeaderLayout>
 );
 
 export default Header;
-
-
