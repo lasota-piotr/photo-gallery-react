c485ecb4acc7885cf7b392bfa8e7af88da44781b
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 10:08:01 +0100

    Add themeApp constant

--- src/stylesUtils/styleConstants.js
+++ src/stylesUtils/styleConstants.js
@@ -4,3 +4,16 @@ export const mqBreakPoints = {
   desktop: 980,
   wide: 1300,
 };
+
+export const themeApp = {
+  unit: '16px',
+  unitXxtiny: '2px',
+  unitXtiny: '4px',
+  unitTiny: '8px',
+  unitSmall: '12px',
+  unitLarge: '24px',
+  unitXlarge: '32px',
+  unitXxlarge: '48px',
+  unitXxxlarge: '56px',
+  unitHuge: '64px',
+};
