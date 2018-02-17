2a23fde982a31e828de8a26a961bd790fa12fa92
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 12:51:00 +0100

    Prettify code

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,10 +7,10 @@ module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: true,
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
-    })
-  ]
+    }),
+  ],
 });
