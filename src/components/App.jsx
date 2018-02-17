f255801c87366c1a4247a780a7433ae60e9ce4ae
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 10:06:51 +0100

    Simplify entry file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './AppRouter';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import Nav from './Nav';
+import CollectionsSwitch from './CollectionsSwitch';
+import { themeApp } from '../stylesUtils/styleConstants';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const App = () => (
+  <ThemeProvider theme={themeApp}>
+    <Router>
+      <Fragment>
+        <Nav />
+        <Route component={CollectionsSwitch} />
+      </Fragment>
+    </Router>
+  </ThemeProvider>
+);
+
+export default App;
