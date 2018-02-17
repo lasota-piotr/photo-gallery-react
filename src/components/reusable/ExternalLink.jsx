0d9536a2e4ac2acdb0ffb487ecb87a71a8e729a6
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 20:23:28 +0100

    Add noUnderline prop

--- src/components/reusable/ExternalLink.jsx
+++ src/components/reusable/ExternalLink.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-const ExternalLink = ({ to, children }) => (
-  <a href={to} target="_blank" rel="noopener">
+const ExternalLinkLayout = styled.a`
+  &:hover {
+    ${props =>
+      props.noUnderline &&
+      css`
+        text-decoration: none;
+      `};
+  }
+`;
+
+const ExternalLink = ({ to, children, noUnderline }) => (
+  <ExternalLinkLayout
+    href={to}
+    target="_blank"
+    rel="noopener"
+    noUnderline={noUnderline}
+  >
     {children}
-  </a>
+  </ExternalLinkLayout>
 );
 
 ExternalLink.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  noUnderline: PropTypes.bool,
+};
+
+ExternalLink.defaultProps = {
+  noUnderline: false,
 };
 
 export default ExternalLink;
