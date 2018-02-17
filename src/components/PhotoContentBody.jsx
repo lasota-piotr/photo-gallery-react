9cff573f38946dcaaf8c18b26c963b8fa3334147
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-17 10:30:55 +0100

    Add react-placeholder dependency

--- src/components/PhotoContentBody.jsx
+++ src/components/PhotoContentBody.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FacebookProvider, { Share, Like } from 'react-facebook';
+import ReactPlaceholder from 'react-placeholder';
 import { facebookAppId } from '../../data';
 import PhotoAuthor from './PhotoAuthor';
 import ExternalLink from './reusable/ExternalLink';
@@ -28,28 +29,41 @@ const PhotoContentBody = ({ photoInfoCollapsed, additionalInfoIsLoading }) => {
         name={user.name || user.username}
       />
       <div>
-        <div>Likes: {likes}</div>
-        <div>
-          Downloads: {!additionalInfoIsLoading ? downloads : <Loading />}
-        </div>
-        <div>Views: {!additionalInfoIsLoading ? views : <Loading />}</div>
         {description && <div>Description: {description}</div>}
-        {categories.length > 0 && <div>Categories: {categories}</div>}
-        {country && <div>Location: {country}</div>}
-        {!additionalInfoIsLoading ? (
-          <div>
-            {exif.make && <div>Camera Make: {exif.make}</div>}
-            {exif.model && <div>Camera Model: {exif.model}</div>}
-            {exif.focal_length && <div>Focal Length: {exif.focal_length}</div>}
-            {exif.aperture && <div>Aperture: {exif.aperture}</div>}
-            {exif.exposure_time && (
-              <div>Shutter Speed: {exif.exposure_time}</div>
-            )}
-            {exif.iso && <div>ISO: {exif.iso}</div>}
-          </div>
-        ) : (
-          <Loading />
-        )}
+        <div>Likes: {likes}</div>
+        <ReactPlaceholder
+          type="text"
+          ready={!additionalInfoIsLoading}
+          rows={3}
+          color="#E0E0E0"
+        >
+          <div>Downloads: {downloads}</div>
+          <div>Views: {views}</div>
+          {categories.length > 0 && <div>Categories: {categories}</div>}
+          {country && <div>Location: {country}</div>}
+        </ReactPlaceholder>
+        <ReactPlaceholder
+          type="text"
+          ready={!additionalInfoIsLoading}
+          rows={6}
+          color="#E0E0E0"
+        >
+          {exif && (
+            <div>
+              {exif.make && <div>Camera Make: {exif.make}</div>}
+              {exif.model && <div>Camera Model: {exif.model}</div>}
+              {exif.focal_length && (
+                <div>Focal Length: {exif.focal_length}</div>
+              )}
+              {exif.aperture && <div>Aperture: {exif.aperture}</div>}
+              {exif.exposure_time && (
+                <div>Shutter Speed: {exif.exposure_time}</div>
+              )}
+              {exif.iso && <div>ISO: {exif.iso}</div>}
+            </div>
+          )}
+        </ReactPlaceholder>
+
         <ExternalLink to={links.download}>Download</ExternalLink>
         <ExternalLink to={links.html}>View image on Unsplash.com</ExternalLink>
       </div>
