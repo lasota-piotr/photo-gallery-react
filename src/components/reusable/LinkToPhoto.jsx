f5708040c2b6e1426241b4ede6b33c4fd7862004
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-18 12:41:03 +0100

    Reformat code

--- src/components/reusable/LinkToPhoto.jsx
+++ src/components/reusable/LinkToPhoto.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const LinkToPhoto = ({
-  children,
-  id,
-  state,
-}) => (
+const LinkToPhoto = ({ children, id, state }) => (
   <Link
     to={{
       pathname: `/photos/${id}`,
