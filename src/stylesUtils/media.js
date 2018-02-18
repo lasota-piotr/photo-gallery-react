55bfa1d69a80a82c03c4b8b2129101e28d4dcff7
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-18 12:20:35 +0100

    Reformat code

--- src/stylesUtils/media.js
+++ src/stylesUtils/media.js
@@ -12,7 +12,6 @@ const Container = styled.div`
 `
 * */
 
-
 const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
