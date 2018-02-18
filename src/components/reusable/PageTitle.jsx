ed16b4cb08044f81655867a457a1cb72b6c90875
Piotr Lasota <lasota.piotr1@gmail.com>
2018-02-18 09:40:45 +0100

    Add PageTitle component

---
+++ src/components/reusable/PageTitle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const PageTitle = styled.h1`
+  padding-top: ${props => props.theme.unitLarge};
+  display: flex;
+  justify-content: center;
+`;
+
+export default PageTitle;
